c6a2d1d23b6e8401bf39a02635ac13823cfc5813
keremturgutlu <keremturgutlu@gmail.com>
2018-03-11 08:43:59 +0300

    version 0.3

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-     name='dicom-contour',    
-     version='0.1',
+     name='dicom_contour',    
+     version=0.3,
      description='A library which converts DICOM images and contours into numpy arrays',
      author='Kerem Turgutlu',
      author_email='kcturgutlu@dons.usfca.edu',
      license='MIT',
+     url='https://github.com/KeremTurgutlu/dicom-contour',
      install_requires=
-	['dicom', 'numpy', 'scipy', 'matplotlib', 'collections', 'os', 'shutil', 'operator','warnings'],
+	['pydicom', 'numpy', 'scipy', 'matplotlib'],
      keywords=['dicom', 'contour', 'medical image'],
-     python_requires='>=3'
+     
 )
